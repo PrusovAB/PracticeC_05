4b0783060ff2a7f27b2810fc541254dda4e3cd19
PrusovAB <djromeo93@mail.ru>
2022-08-06 13:19:03 +0300

    add task27

--- Task_27/Program.cs
+++ Task_27/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Метот CreateArrayRndInd создает случайный массив цифр, 
+// которые указал пользователь,
+// размер массива, максимальное и минимальное значения чисел массива.
+
+int[] CreateArrayRndInd (int size, int min, int max)
+{
+	int[] arr = new int[size];
+	Random rnd = new Random();
+	
+	for(int i = 0; i < arr.Length; i++)
+	{
+		arr[i] = rnd.Next(min,max + 1);
+	}
+
+	return arr;
+}
+
+// Метот PrintArray выводит на экран созданый случайный массив
+
+void PrintArray(int[] array)
+{
+	for (int i = 0; i < array.Length; i++)
+	{
+		if(i == 0) Console.Write("[");
+		if(i < array.Length - 1) Console.Write(array[i] + ",");
+		else Console.Write(array[i] + "]");
+	}
+}
+
+int GetSumNegItem(int[] array){
+
+	int sumItems = 0;
+
+	for (int i = 1; i < array.Length; i++)
+	{
+		if (i % 2 != 0){
+			sumItems += array[i];
+		}
+	}
+	return sumItems;
+}
+
+// Вызов методов.
+int[] array = CreateArrayRndInd(10,0,10); // Задаем массив из 10 элементов,числа рандомные до 10 вкл.
+PrintArray(array); 
+Console.WriteLine(); 
+
+int sumItems = GetSumNegItem(array);
+
+Console.WriteLine($"Сумма не четных элементов массива = {sumItems}");
