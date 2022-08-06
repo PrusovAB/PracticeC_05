fcc9b3a58fa036733b28a77acae215bd93a78907
PrusovAB <djromeo93@mail.ru>
2022-08-06 12:41:31 +0300

    addes task25

--- Task_25/Program.cs
+++ Task_25/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] CreateArrayRndInd (int size, int min, int max)
+{
+	int[] arr = new int[size];
+	Random rnd = new Random();
+	
+	for(int i = 0; i < arr.Length; i++)
+	{
+		arr[i] = rnd.Next(min,max + 1);
+	}
+
+	return arr;
+}
+void PrintArray(int[] array)
+{
+	for (int i = 0; i < array.Length; i++)
+	{
+		if(i == 0) Console.Write("[");
+		if(i < array.Length - 1) Console.Write(array[i] + ",");
+		else Console.Write(array[i] + "]");
+	}
+}
+
+
+void ShowSumPos (int[] array)
+{
+	int sum = 0;
+
+	for (int i = 0; i < array.Length; i++)
+	{
+		if(array[i] % 2 == 0){
+			sum++;
+		}
+	}
+	Console.WriteLine($"В данном массиве количество четных чисел = {sum} ");
+}
+
+int[] array = CreateArrayRndInd(20,100,999);
+PrintArray(array);
+Console.WriteLine();
+ShowSumPos(array);
+
+
+
